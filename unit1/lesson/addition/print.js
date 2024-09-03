23f4dc8212d5cf2c6906c960c5295783d0020dd2
RBLucky <nkosi.s.lucky@gmail.com>
2024-09-03 13:51:56 +0200

    first commit

---
+++ unit1/lesson/addition/print.js
@@ -0,0 +1,3 @@
+const addModule = require("./add");
+
+console.log(addModule.addNum(100, 50));
---
+++ unit1/lesson3/printMessages.js
@@ -0,0 +1,4 @@
+// Require messages module (gives access to messages array)
+const messageModule = require("./messages");
+
+messageModule.messages.forEach(m => console.log(m));
