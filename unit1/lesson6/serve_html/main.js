aa91d8a62ce09476912089bb01d9fa5004f44fd5
RBLucky <nkosi.s.lucky@gmail.com>
2024-09-04 09:34:01 +0200

    lesson6

--- unit1/lesson6/serve_html/main.js
+++ unit1/lesson6/serve_html/main.js
@@ -5,19 +5,22 @@ const fs = require("fs");
 const routeMap = {
     "/": "views/index.html"
 };
-http
-    .createServer((req, res) => {
-        res.writeHead(httpStatus.OK, {
-            "Content-Type": "text/html"
-        });
-        if (routeMap[req.url]) {
-            fs.readFile(routeMap[req.url], (error, data) => {
-                res.write(data);
-                res.end();
-            });
+const getViewUrl = (url) => {
+    return `views${url}.html`;
+};
+http.createServer((req, res) => {
+    let viewUrl = getViewUrl(req.url);
+    fs.readFile(viewUrl, (error, data) => {
+        if (error) {
+            res.writeHead(httpStatus.NOT_FOUND);
+            res.write("<h1>FILE NOT FOUND</h1>");
         } else {
-            res.end("<h1>Sorry, not found.</h1>");
+            res.writeHead(httpStatus.OK, {
+                "Content-Type": "text/html"
+            });
+            res.write(data);
         }
-    })
-    .listen(port);
+        res.end();
+    });
+}).listen(port);
 console.log(`The server has started and is listening on port number: ${port}`);
