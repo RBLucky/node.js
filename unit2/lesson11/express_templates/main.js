427a28f643d2324c98c3e37851f46a761984df6b
RBLucky <nkosi.s.lucky@gmail.com>
2024-09-06 10:13:46 +0200

    lesson11

--- unit2/lesson11/express_templates/main.js
+++ unit2/lesson11/express_templates/main.js
@@ -1,7 +1,8 @@
-const express = require("express"),
-    app = express(),
-    homeController = require("./controllers/homeController"),
-    layouts = require("express-ejs-layouts");
+const express = require("express");
+const app = express();
+const homeController = require("./controllers/homeController");
+const layouts = require("express-ejs-layouts");
+const errorController = require("./controllers/errorController");
 
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
@@ -28,6 +29,10 @@ app.post("/", (req, res) => {
     console.log(req.query);
     res.send("POST Successful!");
 });
+
+//app.use(errorController.logErrors);
+app.use(errorController.respondNoResourceFound);
+app.use(errorController.respondInternalError);
 
 app.listen(app.get("port"), () => {
     console.log(`Server running at http://localhost:${app.get("port")}`);
