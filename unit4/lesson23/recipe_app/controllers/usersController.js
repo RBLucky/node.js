2531b48fa77cb8347d908db1dcd76e4fb85b33ae
RBLucky <nkosi.s.lucky@gmail.com>
2024-09-18 12:39:23 +0200

    lesson11

--- unit4/lesson23/recipe_app/controllers/usersController.js
+++ unit4/lesson23/recipe_app/controllers/usersController.js
@@ -50,7 +50,7 @@ module.exports = {
         req.flash(
           "error",
           `Failed to create user account because: ${error.message}.`
-        ); 
+        );
         next();
       });
   },
@@ -121,6 +121,30 @@ module.exports = {
       .catch(error => {
         console.log(`Error deleting user by ID: ${error.message}`);
         next();
+      });
+  },
+  login: (req, res) => {
+    res.render("users/login");
+  },
+  authenticate: (req, res, next) => {
+    User.findOne({
+      email: req.body.email
+    })
+      .then(user => {
+        if (user && user.password === req.body.password) {
+          res.locals.redirect = `/users/${user._id}`;
+          req.flash("success", `${user.fullName}'s logged in successfully!`);
+          res.locals.user = user;
+          next();
+        } else {
+          req.flash("error", "Your account or password is incorrect. Please try again or contact your system administrator!");
+          res.locals.redirect = "/users/login";
+          next();
+        }
+      })
+      .catch(error => {
+        console.log(`Error logging in user: ${error.message}`);
+        next(error);
       });
   }
 };
