228985d2ab73617c0b3c3f36d6e0de0a2bd4b966
RBLucky <nkosi.s.lucky@gmail.com>
2024-09-20 10:35:00 +0200

    lesson11

--- unit4/lesson24/recipe/controllers/usersController.js
+++ unit4/lesson24/recipe/controllers/usersController.js
@@ -41,21 +41,20 @@ module.exports = {
     res.render("users/new");
   },
   create: (req, res, next) => {
-    let userParams = getUserParams(req.body);
-    User.create(userParams)
-      .then(user => {
-        req.flash("success", `${user.fullName}'s account was created successfully!`);
-        res.locals.redirect = "/users";
-        res.locals.user = user;
-        next();
-      })
-      .catch(error => {
-        console.log(`Error saving user: ${error.message}`);
-        res.locals.redirect = "/users/new";//remains on creation form
-        req.flash("error", `Failed to create user account because: ${error.message}.`)
-        next();
-      });
-  },
+    if (req.skip) return next();
+    let newUser = new User( getUserParams(req.body) );
+    User.register(newUser, req.body.password, (error, user) => { //Register new User
+    if (user) {
+   req.flash("success", `${user.fullName}'s account created successfully!`);
+   res.locals.redirect = "/users";
+   next(); // Set redirect for successful creation
+    } else {
+   req.flash("error", `Failed to create user account because: ${error.message}.`);
+   res.locals.redirect = "/users/new";
+   next(); // Set redirect and log errors in flash messages
+    }
+    });
+   },
   redirectView: (req, res, next) => {
     let redirectPath = res.locals.redirect;
     if (redirectPath) res.redirect(redirectPath);
