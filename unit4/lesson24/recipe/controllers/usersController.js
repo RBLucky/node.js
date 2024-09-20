b06bae15bcd6f6d4ab37aecbe69b09afa2af55b4
RBLucky <nkosi.s.lucky@gmail.com>
2024-09-20 10:37:29 +0200

    lesson11

--- unit4/lesson24/recipe/controllers/usersController.js
+++ unit4/lesson24/recipe/controllers/usersController.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const User = require("../models/user");
+const passport = require("passport");
 
 //takes a body object as input and returns an object containing
 //specific properties extracted from the body
@@ -127,33 +128,12 @@ module.exports = {
   login: (req, res) => {
     res.render("users/login");
   },
-  authenticate: (req, res, next) => {
-    User.findOne({ email: req.body.email })
-      .then(user => {
-        if (user) {
-          user.passwordComparison(req.body.password)
-            .then(passwordsMatch => {
-              if (passwordsMatch) {
-                res.locals.redirect = `/users/${user._id}`;
-                req.flash("success", `${user.fullName}'s logged in successfully!`);
-                res.locals.user = user;
-              } else {
-                req.flash("error", "Failed to log in user account: Incorrect Password.");
-                res.locals.redirect = "/users/login";
-              }
-              next();
-            });
-        } else {
-          req.flash("error", "Failed to log in user account: User  account not found.");
-          res.locals.redirect = "/users/login";
-          next();
-        }
-      })
-      .catch(error => {
-        console.log(`Error logging in user: ${error.message}`);
-        next(error);
-      });
-  },
+  authenticate: passport.authenticate("local", {
+    failureRedirect: "/users/login",
+    failureFlash: "Failed to login.",
+    successRedirect: "/",
+    successFlash: "Logged in!"
+   }),
   validate: (req, res, next) => {
     req.sanitizeBody("email").normalizeEmail({
       all_lowercase: true
