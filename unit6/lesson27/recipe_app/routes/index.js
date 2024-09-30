22d6ff75740e330553a40efd2a0f8f99aa25d45c
RBLucky <nkosi.s.lucky@gmail.com>
2024-09-30 10:38:30 +0200

    lesson11

--- unit6/lesson27/recipe_app/routes/index.js
+++ unit6/lesson27/recipe_app/routes/index.js
@@ -6,11 +6,13 @@ const router = require('express').Router(),
   courseRoutes = require("./courseRoutes"),
   errorRoutes = require("./errorRoutes"),
   homeRoutes = require("./homeRoutes");
+  const apiRoutes = require("./apiToutes");
 
 router.use("/users", userRoutes);
 router.use("/subscribers", subscriberRoutes);
 router.use("/courses", courseRoutes);
 router.use("/", homeRoutes);
 router.use("/", errorRoutes);
+router.use("/api", apiRoutes);
 
 module.exports = router;
